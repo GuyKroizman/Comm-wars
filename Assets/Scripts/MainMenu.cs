f7caf6e73b9b10926860a70cae6ec47fdd44540a
GuyKroizman <kroizguy@gmail.com>
2018-01-26 22:30:57 +0200

    we

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void ClickStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+}
